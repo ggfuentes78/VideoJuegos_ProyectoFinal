3cf47dd1045316788f0a83a622e38e18a8fd77a8
ggfuentes78 <gg_fuentes78@hotmail.com>
2022-11-07 20:32:59 -0300

    V0.0.5
    Version con Spike Wall

---
+++ Proyecto Final/Assets/Scripts/SpikeBlock.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class SpikeBlock : MonoBehaviour
+{
+
+    public float timer;
+    public float topeArriba;
+    public float topeAbajo;
+    public bool abajo;
+    public float speed;
+
+
+    void Start()
+    {
+     timer=2f;
+     abajo=true;
+     speed=5f;   
+    }
+
+
+    void Update()
+    {
+        timer-=Time.deltaTime;
+        if(timer<=0)
+        {
+            if(abajo){
+                transform.Translate(Vector3.down * Time.deltaTime * speed, Space.World);
+                if(transform.position.y<=topeAbajo)
+                {
+                    timer=2f;
+                    abajo=false;
+                }
+            }else{
+                transform.Translate(Vector3.up * Time.deltaTime * speed , Space.World);
+                if(transform.position.y>=topeArriba)
+                {
+                    timer=2f;
+                    abajo=true;
+                }   
+            }
+        }
+    }
+}
